7be1dbc5124604bd87794211c2796796c48dba96
Anshuman <anshumannrt@gmail.com>
2019-04-23 18:23:25 +0530

    First Commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+     protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+        $this->middleware('guest:writer')->except('logout');
+    }
+
+    public function showAdminLoginForm()
+    {
+       return view('auth.login', ['url' => 'admin']);
+    }
+
+    
+
+    public function adminLogin(Request $request)
+    {
+            $request->validate([
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+
+            return redirect()->intended('/admin');
+        }
+        return redirect('/login/admin')
+        ->with('error','These credentials do not match our records');
+
+    }
+
+
+    public function showWriterLoginForm()
+    {
+        return view('auth.login', ['url' => 'writer']);
+    }
+
+    
+     public function writerLogin(Request $request)
+    {
+        $request->validate([
+            'email'   => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if (Auth::guard('writer')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+
+            return redirect()->intended('/writer');
+        }
+        return redirect('/login/writer')
+        ->with('error','These credentials do not match our records');
+    }
+}
